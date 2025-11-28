87da1e1ee6dea4b1dd7fe56f55328578e9cad274
Sourabh Undale <sourabhundale@gmail.com>
2025-11-28 21:38:23 +0530

    28Nov25

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,9 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+    // get user details from frontend
+    // validation
+    // check if user already exists
+    // check for images, check for avtar
+    // upload them to cloudinary
+    // create user object - create entry in db
+    // remove password and refresh token field form response
+    //check for user creation
+    // return res
+
+
+
+    const {fullName, email, username, password } = req.body
+
+ 
 })
+
+
+
 
 export {registerUser}
